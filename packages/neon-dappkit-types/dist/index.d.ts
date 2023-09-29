fdf3f87ed6e9633a9d76672e4dda56cc898d66b9
Leonardo de Jesus Diz Conde <leonardo.conde@simpli.com.br>
2023-09-29 16:58:49 -0300

    CU-86a0uwqwy - For neon-dappkit and wcsdk: Folders /dist and /docs shouldn't be commited

--- packages/neon-dappkit-types/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './Neo3EventListener';
-export * from './Neo3Invoker';
-export * from './Neo3Parser';
-export * from './Neo3Signer';
-export * from './TypeChecker';
--- packages/neon-dappkit/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './NeonEventListener';
-export * from './NeonInvoker';
-export * from './NeonParser';
-export * from './NeonSigner';
